434efb7b653a994348e3c939cb58d390f2b228bc
Andrew Park <atpark333@gmail.com>
2021-11-05 13:00:03 +0900

    docs: add doc comments to util functions

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
+/**
+ * Generate a range of integers from start to end
+ * @param {Number} start 
+ * @param {Number} end 
+ * @param {Number} step 
+ */
 function* range(start, end, step = 1) {
   let current = start;
   while (current < end) {
@@ -6,6 +12,12 @@ function* range(start, end, step = 1) {
   }
 }
 
+/**
+ * Format the input and time cost as a string
+ * @param {Number} size 
+ * @param {Number} cost 
+ * @returns 
+ */
 function trace(size = 0, cost = 0) {
   return `len: ${size}, time: ${cost}`;
 }
