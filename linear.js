158a6214b5ab4f82387cad6deffc4abbabf620f3
Andrew Park <atpark333@gmail.com>
2021-11-10 17:47:25 +0900

    refactor: update comments and format

--- linear.js
+++ linear.js
@@ -5,22 +5,19 @@ function linearSearch(arr, target) {
   //  TODO: create a counter
 
   for (let i = 0; i < arr.length; i++) {
-
     // TODO: increment the counter
+    console.log(`Item at ${i}`);
 
     if (arr[i] === target) {
       console.log(`Found item: ${target}`);
-      
     }
   }
 
   // TODO: Try adding another operation with count increment
-
   // TODO: Try adding another for loop
 
   console.log(`${target} not found in the array`);
 
-  // TODO: return the trace result
   return trace(arr.length, counter.count);
 }
 
