416fd55df7aaa17f93b7354faf1300ca3adbae8d
Andrew Park <atpark333@gmail.com>
2022-07-14 20:46:24 +0900

    Update user prompt handler

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const prompt = require('prompt-sync')({ sigint: true });
+const constant = require('./src/constant');
+const linear = require('./src/linear');
+const quadratic = require('./src/quadratic');
+const logarithmic = require('./src/logarithmic');
 
-let choice;
-const options = {
-  a: 'constant',
-  b: 'linear',
-  c: 'quadratic',
-  d: 'logarithmic'
-};
+function start() {
+  let choice;
 
-const labels = Object.keys(options);
+  const options = {
+    a: 'constant',
+    b: 'linear',
+    c: 'quadratic',
+    d: 'logarithmic'
+  };
 
-while (!labels.includes(choice)) {
-  console.log('Pick a time complexity:');
-  for (let key in options) {
-    console.log(`${key}) ${options[key]}`);
+  const labels = Object.keys(options);
+
+  while (!labels.includes(choice)) {
+    console.log('Pick a time complexity:');
+    for (let key in options) {
+      console.log(`${key}) ${options[key]}`);
+    }
+    choice = prompt('Which time complexity? ');
   }
-  choice = prompt('Which time complexity? ');
+
+  console.log(`Your choice is ${options[choice]}`);
+
+  switch(options[choice]) {
+    case 'constant':
+      constant.run();
+      break;
+    case 'linear':
+      linear.run();
+      break;
+    case 'quadratic':
+      quadratic.run();
+      break;
+    case 'logarithmic':
+      logarithmic.run();
+      break;
+    default:
+      throw new Error('Invalid choice: ' + choice);
+  }
 }
 
-console.log(`Your choice is ${options[choice]}`);
+start();
