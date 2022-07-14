e7d1adc29388db3cbea6a2738e6dcffec230060b
Andrew Park <atpark333@gmail.com>
2022-07-14 20:33:09 +0900

    Add command line interface

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import quadratic from "./src/quadratic.js";
+const prompt = require('prompt-sync')({ sigint: true });
 
-quadratic([1, 2, 3, 4]);
+let choice;
+const options = {
+  a: 'constant',
+  b: 'linear',
+  c: 'quadratic',
+  d: 'logarithmic'
+};
+
+const labels = Object.keys(options);
+
+while (!labels.includes(choice)) {
+  console.log('Pick a time complexity:');
+  for (let key in options) {
+    console.log(`${key}) ${options[key]}`);
+  }
+  choice = prompt('Which time complexity? ');
+}
+
+console.log(`Your choice is ${options[choice]}`);
