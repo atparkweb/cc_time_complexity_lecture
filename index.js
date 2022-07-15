7d657cd85d5731f9f99266d18ea0a530db3aa6fd
Andrew Park <atpark333@gmail.com>
2022-07-15 09:53:02 +0900

    Add quit option to CLI interface

--- index.js
+++ index.js
@@ -11,36 +11,48 @@ function start() {
     a: 'constant',
     b: 'linear',
     c: 'quadratic',
-    d: 'logarithmic'
+    d: 'logarithmic',
+    q: 'QUIT'
   };
 
   const labels = Object.keys(options);
 
-  while (!labels.includes(choice)) {
-    console.log('Pick a time complexity:');
-    for (let key in options) {
-      console.log(`${key}) ${options[key]}`);
+  let running = true;
+
+  while (running) {
+    while (!labels.includes(choice)) {
+      console.log('Pick a time complexity:');
+
+      for (let key in options) {
+        console.log(`${key}) ${options[key]}`);
+      }
+      choice = prompt('Which time complexity? ');
     }
-    choice = prompt('Which time complexity? ');
-  }
 
-  console.log(`Your choice is ${options[choice]}`);
+    console.clear();
 
-  switch(options[choice]) {
-    case 'constant':
-      constant.run();
-      break;
-    case 'linear':
-      linear.run();
-      break;
-    case 'quadratic':
-      quadratic.run();
-      break;
-    case 'logarithmic':
-      logarithmic.run();
-      break;
-    default:
-      throw new Error('Invalid choice: ' + choice);
+    switch(options[choice]) {
+      case 'constant':
+        constant.run();
+        break;
+      case 'linear':
+        linear.run();
+        break;
+      case 'quadratic':
+        quadratic.run();
+        break;
+      case 'logarithmic':
+        logarithmic.run();
+        break;
+      case 'QUIT':
+        running = false;
+        break;
+      default:
+        console.log('Invalid choice: ' + choice);
+        break;
+    }
+
+    choice = undefined;
   }
 }
 
